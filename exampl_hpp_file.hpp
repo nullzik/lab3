c7626187323916145ae8ba7675e2d4cfd39db437
nullzik <boykocrypto@gmail.com>
2025-12-19 20:08:06 +0700

    Initial commit

---
+++ exampl_hpp_file.hpp
@@ -0,0 +1,36 @@
+class HorseCorral
+{
+public:
+	// публичные: переопределение типом, внутренние нумерованные списки, константы
+	using HorsesType = std::vector<Horse>;
+	
+	enum class Type
+	{
+		eOpen = 0,
+		eClose = 1,
+	};
+	
+	static const int MAX_HORSE = 15;
+
+private:
+	// приватные константы
+	static constexpr char* TAG_FOR_LOG = "name";
+	
+	// приватные переменные
+	HorsesType m_horseArray;
+	int m_countHorses;
+	Type m_currentType;
+
+public:	
+	
+	// конструткоры и деконструктор
+	HorseCorral() = default;	
+	~HorseCorral() = default;
+	
+	// публичные методы
+	void AddHorse(Horse& horse);
+	
+private:
+	// приватные методы
+	void KillHorse(Horse* horseOld);
+};
